34f3712a1bca0f6ed6c85e98b2a63fa143324b24
Bobak Hashemi <bth5032@gmail.com>
2017-09-20 00:21:16 +0200

    added rhythm option, and now all the notes are in C-Major, sounds much nicer

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--prose", help="Text to play", type=str, default="He was alone and young and wilful and wildhearted")
 parser.add_argument("-t", "--tempo", help="Time between notes", type=float, default=0.1)
 parser.add_argument("-c", "--chord", help="Play the first N characters", type=int, default=1)
+parser.add_argument("-r", "--rhythm", help="Use the length of the word to make a rhythm", action='store_true', default=False)
 
 fluidsynth.init("Nice-Keys-B-JNv1.5.sf2")
 
@@ -18,7 +19,7 @@ args=parser.parse_args()
 text=args.prose
 chord=args.chord
 
-letter_map = {
+"""letter_map = {
 "a": "A-4", 
 "b": "A#-4",
 "c": "B-4",
@@ -45,10 +46,50 @@ letter_map = {
 "x": "G#-5",
 "y": "A-6",
 "z": "A#-6",
+}"""
+letter_map = {
+"a": "C-3", 
+"b": "D-3",
+"c": "E-3",
+"d": "F-3",
+"e": "G-3",
+"f": "A-4",
+"g": "B-4",
+"h": "C-4",
+"i": "D-4",
+"j": "E-4",
+"k": "F-4",
+"l": "G-4",
+"m": "A-5",
+"n": "B-5",
+"o": "C-5",
+"p": "D-5",
+"q": "E-5",
+"r": "F-5",
+"s": "G-5",
+"t": "A-6",
+"u": "B-6",
+"v": "C-6",
+"w": "D-6",
+"x": "E-6",
+"y": "F-6",
+"z": "G-6",
 }
-lm = lambda char: letter_map[char.lower()] if char.lower() in letter_map.keys() else "B-6"
+lm = lambda char: letter_map[char.lower()] if char.lower() in letter_map.keys() else "C-5"
 
 tempo=args.tempo
+
+def makePause():
+  def pauseWithRhythm(word):
+    return sleep(min(tempo*len(word), 0.5))
+  def pauseWithoutRhythm(word):
+    return sleep(tempo)
+  
+  if (args.rhythm):
+    return pauseWithRhythm
+  else:
+    return pauseWithoutRhythm
+    
 
 #====================Notes For Clay=======================================
 # If you want to extend, you should probably do two things...
@@ -58,7 +99,9 @@ tempo=args.tempo
 # Have fun!
 #=========================================================================
 
+pause = makePause()
+
 for word in text.split():
   nc = NoteContainer( map(lm , word[:chord]) ) #This line you won't understand Clay, but I can explain if you are interested in playing
   fluidsynth.play_NoteContainer(nc)
-  sleep(tempo)
+  pause(word)
